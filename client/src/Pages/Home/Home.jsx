32a621ce8ebd147f86a3563ce5b2e75e09afc3f9
Rabi3421 <rabinarayan.p@digitalboosts.com>
2023-04-04 17:03:02 +0530

    landing section done

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../../Components/Navbar/Navbar'
+import Card from '../../Components/Card/Card'
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
+        <Card />
     </div>
   )
 }
